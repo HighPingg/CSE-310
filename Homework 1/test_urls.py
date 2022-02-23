f16f50d79ba7899f198fe2757d7a185124cc68f9
Vincent Zheng <zhengvincent0825@gmail.com>
2022-02-22 23:06:05 -0500

    getting experimental data

--- Homework 1/test_urls.py
+++ Homework 1/test_urls.py
@@ -10,33 +10,43 @@ TEST_URLS = [ 'Wikipedia.org'
             , 'Google.com.hk'
             ]
 
-# TESTING MY DIG
-# # from mydig import mydig
+# # TESTING MY DIG
+# from mydig import mydig
 
-# # for url in TEST_URLS:
-# #     mydig(url)
+# for url in TEST_URLS:
+#     for _ in range(10):
+#         mydig(url)
 # OUTPUT FROM HERE WAS REDIRECTED
 
-# # with open('./test_results/mydigresults.txt') as f:
-# #     output = f.read().splitlines()
-# #     output = list(filter(lambda x: 'Query time: ' in x, output))
+# with open('./test_results/mydigresults.txt') as f:
+#     output = f.read().splitlines()
+#     output = list(filter(lambda x: 'Query time: ' in x, output))
 
-# # for i in range(len(output)):
-# #     print(output[i].split(' ')[2], '-', TEST_URLS[i])
+# times = []
+# for i in range(len(output)):
+#     times.append(int(output[i].split(' ')[2]))
+
+#     if i % 10 == 9:
+#         print(times, TEST_URLS[int(i / 10)])
+#         times = []
 
 
-# TESTING LOCAL AND PUBLIC DNS RESOLVER
-import dns.resolver
-from time import time
+# # TESTING LOCAL AND PUBLIC DNS RESOLVER
+# import dns.resolver
+# from time import time
 
-my_resolver = dns.resolver.Resolver()
+# my_resolver = dns.resolver.Resolver()
 
-# 8.8.8.8 is Google's public DNS server
-my_resolver.nameservers = ['8.8.8.8']
+# # 8.8.8.8 is Google's public DNS server
+# #my_resolver.nameservers = ['8.8.8.8']
 
-for domain in TEST_URLS:
-    timeElapsed = time()
-    answer = my_resolver.resolve(domain)
-    timeElapsed = round((time() - timeElapsed) * 1000)
+# for domain in TEST_URLS:
+#     times = []
+#     for _ in range(10):
+#         timeElapsed = time()
+#         answer = my_resolver.resolve(domain)
+#         timeElapsed = round((time() - timeElapsed) * 1000)
+
+#         times.append(timeElapsed)
     
-    print(timeElapsed, '-', domain)
+#     print(times, domain)
