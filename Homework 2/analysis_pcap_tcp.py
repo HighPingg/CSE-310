5c61a0cd9ef46ec9ce77f8099375ea85e6beb83a
Vincent Zheng <zhengvincent0825@gmail.com>
2022-03-08 22:26:04 -0500

    function-ified analysis_pcap_tcp.py

--- Homework 2/analysis_pcap_tcp.py
+++ Homework 2/analysis_pcap_tcp.py
@@ -1,39 +1,42 @@
 import dpkt
 from TCPFlow import TCPFlow
 
-f = open('PCAP files/assignment2.pcap', 'rb')
-pcap = dpkt.pcap.Reader(f)
+def analyze(file: str):
+    f = open(file, 'rb')
+    pcap = dpkt.pcap.Reader(f)
 
-num = 0
-flows = []
+    num = 0
+    flows = []
 
-for ts, buf in pcap:
-    # if num < 50:
-    #     num += 1
-    #     print(ts)
-    
-    ip = dpkt.ethernet.Ethernet(buf).data
+    for ts, buf in pcap:
+        # if num < 50:
+        #     num += 1
+        #     print(ts)
+        
+        ip = dpkt.ethernet.Ethernet(buf).data
 
-    # Check to make sure it's a TCP connection.
-    if ip.p != dpkt.ip.IP_PROTO_TCP:
-        continue
+        # Check to make sure it's a TCP connection.
+        if ip.p != dpkt.ip.IP_PROTO_TCP:
+            continue
 
-    # Check if ip already in flows
-    newFlow = True
+        # Check if ip already in flows
+        newFlow = True
+        for flow in flows:
+            if flow.belongsIn(ip):
+                flow.addPacket(ts, ip)
+                newFlow = False
+                break
+
+        if newFlow and (ip.data.flags & dpkt.tcp.TH_SYN):
+            flows.append(TCPFlow(ts, ip))
+
     for flow in flows:
-        if flow.belongsIn(ip):
-            flow.addPacket(ts, ip)
-            newFlow = False
-            break
-
-    if newFlow:
-        flows.append(TCPFlow(ts, ip))
-
-for flow in flows:
-    print(flow, '\n\n')
-    
-    # for ip in flow.pastPacks:
-    #     print('\t', TCPFlow.getTCPInfo(ip.data))
+        print(flow, '\n\n')
+        
+        # for ip in flow.pastPacks:
+        #     print('\t', TCPFlow.getTCPInfo(ip.data))
 
 
-f.close()
+    f.close()
+
+analyze('PCAP files/assignment2.pcap')
